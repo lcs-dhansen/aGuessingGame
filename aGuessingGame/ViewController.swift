efa4f307be9c0ca54027d8cb0023347bdcc937c2
Devon Hanses <dhansen@lcs.on.ca>
2019-10-31 16:09:31 -0400

    Initial Commit

---
+++ aGuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aGuessingGame
+//
+//  Created by Devon Hansen on 2019-10-31.
+//  Copyright © 2019 Devon Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
